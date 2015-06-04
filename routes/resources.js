a1d3ccd3eededf9d3c4c889b712fb9fa32894514
Weston Renoud <wrenoud@gmail.com>
2015-06-04 01:31:52 -0300

    more resource def interactions

--- routes/resources.js
+++ routes/resources.js
@@ -11,17 +11,32 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  var definition;
   req.models.ResourceDefinition.create({'name':req.body.name})
     .then(function(def){
+      definition = def;
       var properties = req.body.properties;
-      for( key in properties){
-        console.log(properties[key]);
+      for(key in properties){
+        properties[key]['ResourceDefinitionId'] = def.id
       }
-
+      return req.models.PropertyDefinition.bulkCreate(properties);
+    //}).then(function(properties){
+    //  return definition.addPropertyDefinitions(properties);
+    }).then(function(){
       return req.models.ResourceDefinition.findAll();
     }).then(function(defs){
       res.render('resources', { 'ResourceDefinitions': defs });
     });
 });
 
+router.get('/:id', function(req, res, next) {
+  console.log(req.params);
+  req.models.ResourceDefinition.findOne({'where': {'id': req.params.id}, 'include':[req.models.PropertyDefinition]})
+    .then(function(def){
+      console.log(def);
+      return req.models.ResourceDefinition.findAll();
+    }).then(function(defs){
+      res.render('resources', { 'ResourceDefinitions': defs });
+    });
+});
 module.exports = router;
